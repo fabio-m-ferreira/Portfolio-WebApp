7bd0886544eb25e7abd1ec94caca8ef54c91367a
Fábio Ferreira <101990785+fabio-m-ferreira@users.noreply.github.com>
2025-02-28 14:23:50 +0000

    removed unused Link import
    Edited with Github editor

--- src/sections/footer/Footer.tsx
+++ src/sections/footer/Footer.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./footer.module.css";
 import { BsGithub, BsLinkedin, BsMedium } from "react-icons/bs";
 import { FaEnvelope } from "react-icons/fa6";
-import Link from "next/link";
 
 const Footer = () => {
   return (
